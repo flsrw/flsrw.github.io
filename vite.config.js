ed129c0d344afc981695e3ef5b17b3a539b8d2af
flsrw <srw.log@gmail.com>
2023-07-22 00:40:33 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 const path = require('path')
 
 export default defineConfig({
-    base: 'home',
+    base: '/home/',
     plugins: [react()],
     root: path.resolve(__dirname, 'src'),
     build: {
