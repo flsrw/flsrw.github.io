3626a3859b74d68db15f5c56d184cce9d3f4768c
flsrw <srw.log@gmail.com>
2023-07-22 00:32:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//     base: 'home',
-//     plugins: [react()]
-// })
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 const path = require('path')
 
-export default {
-  root: path.resolve(__dirname, 'src'),
+export default defineConfig({
+    base: 'home',
+    plugins: [react()],
+    root: path.resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
   }
-}
+})
